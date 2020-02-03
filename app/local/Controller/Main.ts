8c45b439605010ffd99fedacd1e780a962606083
Jasper Stafleu <j.stafleu@developers.nl>
2020-02-03 16:59:27 +0100

    Minor ommission fixed

--- app/local/Controller/Main.ts
+++ app/local/Controller/Main.ts
@@ -1,14 +1,9 @@
 import {IncomingMessage} from "http";
 import Controller from "$stafleu/Controller/Controller";
-import TemplateEngine from "$stafleu/Templating/Engine";
-import View from "$stafleu/Component/View";
+import View from "$s`tafleu/Component/View";
 
 export default class Main implements Controller
 {
-    constructor(protected templating: TemplateEngine)
-    {
-    }
-
     index(name: string, request: IncomingMessage): View
     {
         return new View('local/template/helloWorld.html.jstpl', {
